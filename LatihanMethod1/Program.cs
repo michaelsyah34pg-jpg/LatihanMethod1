68f333c2ba2672c315b3ed1c68921c44d91ae02c
michaelsyah34pg-jpg <michael_syah_34pg@student.smktelkom-mlg.sch.id>
2026-01-14 09:12:31 +0700

    menambahkan parameter dan argument

--- LatihanMethod1/Program.cs
+++ LatihanMethod1/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿static void tambah(int a, int b)
+{
+    int c = 0;
+
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+
+static void kurang(int a, int b)
+{
+    int  c = 0;
+
+    c = a + b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+
+static void kali(int a, int b)
+{
+    int c = 0;
+
+    c = a + b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+
+static void bagi(int a, int b)
+{
+    int c = 0;
+
+    c = a + b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+tambah(5,3);
+kurang(1,1);
+kali(7,6);
+bagi(8,2);
